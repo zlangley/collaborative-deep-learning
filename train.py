78c5a5ae5a63522e405ec408e5bee28fa77de26c
Zach Langley <zblangley@gmail.com>
2021-04-30 10:47:11 -0400

    Run coordinate ascent on the CPU

--- train.py
+++ train.py
@@ -1,4 +1,5 @@
 import torch
+import torch.cuda
 from torch import linalg
 from torch import autograd
 from torch.utils.data import DataLoader
@@ -27,17 +28,23 @@ def train_cdl(cdl, content_dataset, ratings_matrix, optimizer, conf, epochs, bat
     print('Training CDL')
     for epoch in range(epochs):
         print('Epoch', epoch + 1)
-        num_iters = 1
+
+        # Each epoch is one iteration of coordinate ascent (for U and V)
+        # followed by one iteration of gradient descent (for the SDAE
+        # parameters).
 
         with autograd.no_grad():
+            print('  running coordinate ascent...')
             coordinate_ascent(cdl, ratings_matrix, conf, encoded_dataset, num_iters)
 
-        # This training will only tweak the SDAE parameters, but it will use the weights
-        # U and V in the loss.
+        # The parameters U and V are not updated below since they have
+        # require_grad=False.  However, their values will influence the loss
+        # and therefore the gradients of the SDAE.
+        print('  running gradient descent...')
         train(cdl, content_dataset, batch_size, cdl_loss, optimizer)
 
 
-def coordinate_ascent(cdl, R, conf, enc, num_iters):
+def coordinate_ascent(cdl, R, conf, enc, num_iters=1):
     """
     :param U: The latent users matrix of shape (num_users, latent_size).
     :param V: The latent items matrix of shape (num_items, latent_size).
@@ -45,7 +52,15 @@ def coordinate_ascent(cdl, R, conf, enc, num_iters):
     :param C: The confidence matrix of shape (num_users, num_items).
     :param enc: The encodings of the content of shape (num_items, latent_size).
     """
-    latent_size = cdl.U.shape[1]
+    # Because of the iterations below, coordinate ascent is much faster on the CPU.
+    orig_device = cdl.U.device
+
+    V = cdl.V.data.to('cpu')
+    U = cdl.U.data.to('cpu')
+    R = R.to('cpu')
+    enc = enc.to('cpu')
+
+    latent_size = U.shape[1]
     idu = cdl.lambda_u * torch.eye(latent_size, device=R.device)
     idv = cdl.lambda_v * torch.eye(latent_size, device=R.device)
     conf_a, conf_b = conf
@@ -53,38 +68,35 @@ def coordinate_ascent(cdl, R, conf, enc, num_iters):
     scaled_enc = enc * cdl.lambda_v
 
     for i in range(num_iters):
-        print('Updated U...')
-        VtV = conf_b * cdl.V.t() @ cdl.V
-        for j in range(len(cdl.U)):
+        VtV = conf_b * V.t() @ V
+        for j in range(len(U)):
             rated_idx = R[j].nonzero().squeeze(1)
-            Vr = cdl.V[rated_idx, :]
+            Vr = V[rated_idx, :]
             A = VtV + (conf_a - conf_b) * Vr.t() @ Vr + idu
             b = Vr.t() @ R[j, rated_idx]
-#            VC = cdl.V.t() * C[j]
-#            A = VC @ cdl.V + idu
+#            VC = V.t() * C[j]
+#            A = VC @ V + idu
 #            b = VC @ R[j]
-            cdl.U[j] = linalg.solve(A, b)
+            U[j] = linalg.solve(A, b)
 
-        print('Updated U.')
-        print('Updating V...')
-
-        UtU = conf_b * cdl.U.t() @ cdl.U
-        for j in range(len(cdl.V)):
+        UtU = conf_b * U.t() @ U
+        for j in range(len(V)):
             A = UtU + idv
             b = scaled_enc[j]
 
             rated_idx = R[:, j].nonzero().squeeze(1)
             if len(rated_idx):
-                Ur = cdl.U[rated_idx, :]
+                Ur = U[rated_idx, :]
                 A += (conf_a - conf_b) * Ur.t() @ Ur
                 b += Ur.t() @ R[rated_idx, j]
 
             #UC = cdl.U.t() * C[:, j]
-            #A = UC @ cdl.U + idv
+            #A = UC @ U + idv
             #b = UC @ R[:, j] + scaled_enc[j]
-            cdl.V[j] = linalg.solve(A, b)
+            V[j] = linalg.solve(A, b)
 
-        print('Updated V.')
+    cdl.V.data = V.to(orig_device)
+    cdl.U.data = U.to(orig_device)
 
 
 def train_sdae(sdae, dataset, loss_fn, optimizer, epochs, batch_size):
