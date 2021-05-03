eddd1329090890d5c5fe929acf978d64693e1f62
Zach Langley <zblangley@gmail.com>
2021-05-02 21:31:18 -0400

    Dropout/activate between all layers of SDAE

--- sdae.py
+++ sdae.py
@@ -7,8 +7,27 @@ class StackedAutoencoder(nn.Module):
 
         self.autoencoders = autoencoder_stack
 
-        self.encode = nn.Sequential(*[autoencoder.encode for autoencoder in self.autoencoders])
-        self.decode = nn.Sequential(*[autoencoder.decode for autoencoder in reversed(self.autoencoders)])
+        encoder_modules = []
+        decoder_modules = []
+
+        for autoencoder in self.autoencoders[:-1]:
+            encoder_modules.append(autoencoder._encode)
+            encoder_modules.append(autoencoder._activation)
+            encoder_modules.append(autoencoder._dropout)
+
+        # No dropout at the last encoding step.
+        encoder_modules.append(self.autoencoders[-1]._encode)
+        encoder_modules.append(self.autoencoders[-1]._activation)
+        decoder_modules.append(self.autoencoders[-1]._dropout)
+        decoder_modules.append(self.autoencoders[-1]._decode)
+
+        for autoencoder in reversed(self.autoencoders[:-1]):
+            decoder_modules.append(autoencoder._activation)
+            decoder_modules.append(autoencoder._dropout)
+            decoder_modules.append(autoencoder._decode)
+
+        self.encode = nn.Sequential(*encoder_modules)
+        self.decode = nn.Sequential(*decoder_modules)
 
         self.weights = [weight for ae in self.autoencoders for weight in ae.weights]
         self.biases = [bias for ae in self.autoencoders for bias in ae.biases]
@@ -33,21 +52,23 @@ class Autoencoder(nn.Module):
         :param in_features: The number of features (rows) of the input.
         :param latent_size: The size of the latent representation.
         :param dropout: The dropout probability before decoding.
-        :param activiation: The activation function.
+        :param activation: The activation function.
         :param tie_weights: Whether to use the same weight matrix in the encoder and decoder.
         """
         super().__init__()
 
-        encode = nn.Linear(in_features, latent_size)
-        decode = nn.Linear(latent_size, in_features)
+        self._encode = nn.Linear(in_features, latent_size)
+        self._decode = nn.Linear(latent_size, in_features)
+        self._activation = activation
+        self._dropout = nn.Dropout(dropout)
 
         if tie_weights:
-            decode.weight.data = encode.weight.t()
-            self.weights = [encode.weight]
+            self._decode.weight.data = self._encode.weight.t()
+            self.weights = [self._encode.weight]
         else:
-            self.weights = [encode.weight, decode.weight]
+            self.weights = [self._encode.weight, self._decode.weight]
 
-        self.biases = [encode.bias.data, decode.bias.data]
+        self.biases = [self._encode.bias.data, self._decode.bias.data]
 
         for weight in self.weights:
             nn.init.xavier_uniform_(weight)
@@ -55,14 +76,8 @@ class Autoencoder(nn.Module):
         for bias in self.biases:
             nn.init.zeros_(bias)
 
-        self.encode = nn.Sequential(
-            encode,
-            activation,
-        )
-        self.decode = nn.Sequential(
-            nn.Dropout(dropout),
-            decode,
-        )
+        self.encode = nn.Sequential(self._encode, self._activation)
+        self.decode = nn.Sequential(self._dropout, self._decode)
 
     def forward(self, x):
         latent = self.encode(x)
