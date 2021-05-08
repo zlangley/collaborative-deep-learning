Fix loss calculation on gpu

train.py
@@ -40,9 +40,9 @@ def train_model(sdae, lfm, content, ratings, optimizer, recon_loss_fn, config, e
         lfm_optim.step(latent_items_target)
 
         if epoch % 3 == 0:
-            loss = lfm_optim.loss(latent_items_target)
-            loss += config['lambda_n'] / 2 * F.mse_loss(recon, content, reduction='sum')
-            loss += config['lambda_w'] / 2 * (sum(w.square().sum() for w in sdae.weights) + sum(b.square().sum() for b in sdae.biases))
+            loss = lfm_optim.loss(latent_items_target).item()
+            loss += config['lambda_n'] / 2 * F.mse_loss(recon, content, reduction='sum').item()
+            loss += config['lambda_w'] / 2 * (sum(w.square().sum() for w in sdae.weights) + sum(b.square().sum() for b in sdae.biases)).item()
             logging.info(f'  neg_likelihood: {loss}')
 
         # Update SDAE weights. Loss here only depends on SDAE outputs.