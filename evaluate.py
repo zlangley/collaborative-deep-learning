b726d8e36448c34911c8b568daf3ef19c9810cd5
Zach Langley <zblangley@gmail.com>
2021-05-10 18:06:44 -0400

    Use torch.topk

--- evaluate.py
+++ evaluate.py
@@ -1,12 +1,9 @@
 import torch
 
 
-def recall(pred, test, M):
-    _, indices = torch.sort(pred, dim=1, descending=True)
 
-    top = indices[:, :M]
-
-    gathered = test.gather(1, top)
+def recall(pred, test, k):
+    _, indices = torch.topk(pred, k)
+    gathered = test.gather(1, indices)
     recall = gathered.sum(dim=1) / test.sum(dim=1)
-
     return recall.mean()
