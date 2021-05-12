e50be0e48eaf18bbfce97988e8604c67a48043d8
Zach Langley <zblangley@gmail.com>
2021-05-12 09:51:37 -0400

    More refactoring

--- cdl/autoencoder.py
+++ cdl/autoencoder.py
@@ -1,6 +1,4 @@
-import torch
 from torch import nn
-import torch.nn.functional as F
 
 
 class StackedAutoencoder(nn.Module):
@@ -19,14 +17,14 @@ class StackedAutoencoder(nn.Module):
             x = ae.encode(x)
 
             if i != len(self.stack) - 1:
-                x = ae._dropout(x)
+                x = ae.dropout(x)
 
         return x
 
     def decode(self, x):
         for i, ae in enumerate(reversed(self.stack)):
             if i != 0:
-                x = ae._activation(x)
+                x = ae.activation(x)
 
             x = ae.decode(x)
 
@@ -46,35 +44,25 @@ class Autoencoder(nn.Module):
         """
         super().__init__()
 
-        self._weight_enc = nn.Parameter(torch.Tensor(latent_size, in_features))
+        encode = nn.Linear(in_features, latent_size)
+        decode = nn.Linear(latent_size, in_features)
 
         if tie_weights:
-            self._weight_dec = nn.Parameter(self._weight_enc.t())
-        else:
-            self._weight_dec = nn.Parameter(torch.Tensor(in_features, latent_size))
+            decode.weight.data = encode.weight.data.t()
 
-        self._bias_enc = nn.Parameter(torch.Tensor(latent_size))
-        self._bias_dec = nn.Parameter(torch.Tensor(in_features))
+        self.dropout = nn.Dropout(dropout)
+        self.activation = activation
 
-        self._activation = activation
-        self._dropout = nn.Dropout(dropout)
-
-        nn.init.xavier_normal_(self._weight_enc)
-        nn.init.xavier_normal_(self._weight_dec)
-        nn.init.zeros_(self._bias_enc)
-        nn.init.zeros_(self._bias_dec)
+        self.encode = nn.Sequential(
+            encode,
+            self.activation,
+        )
+        self.decode = nn.Sequential(
+            self.dropout,
+            decode,
+        )
 
     def forward(self, x):
         latent = self.encode(x)
         reconstructed = self.decode(latent)
         return latent, reconstructed
-
-    def encode(self, x):
-        x = F.linear(x, self._weight_enc, self._bias_enc)
-        x = self._activation(x)
-        return x
-
-    def decode(self, x):
-        x = self._dropout(x)
-        x = F.linear(x, self._weight_dec, self._bias_dec)
-        return x
